91426684c118c6a46b32cdfda85d1aff69631941
Valerij_Gorohov <gorohov.valerij@whitetigersoft.ru>
2021-05-19 16:15:42 +0300

    rebove backdrop

--- lib/nav-bar-styles/wts-style-1-bottom-nav-bar.widget.dart
+++ lib/nav-bar-styles/wts-style-1-bottom-nav-bar.widget.dart
@@ -3,13 +3,8 @@ part of persistent_bottom_nav_bar;
 class BottomNavStyleWTS1 extends StatelessWidget {
   final NavBarEssentials? navBarEssentials;
 
-  final double sigmaX;
-  final double sigmaY;
-
   BottomNavStyleWTS1({
     Key? key,
-    this.sigmaX = 5.0,
-    this.sigmaY = 5.0,
     this.navBarEssentials = const NavBarEssentials(items: null),
   });
 
@@ -88,25 +83,37 @@ class BottomNavStyleWTS1 extends StatelessWidget {
                 (this.navBarEssentials!.padding?.right ??
                     MediaQuery.of(context).size.width * 0.05))) /
         this.navBarEssentials!.items!.length);
-    return BackdropFilter(
-      filter: ImageFilter.blur(sigmaX: sigmaX, sigmaY: sigmaY),
-      child: Container(
-        decoration: BoxDecoration(color: Color(0xfff4faff).withOpacity(0.88)),
-        width: double.infinity,
-        height: this.navBarEssentials!.navBarHeight,
-        padding: EdgeInsets.only(
-            top: this.navBarEssentials!.padding?.top ?? 0.0,
-            left: this.navBarEssentials!.padding?.left ??
-                MediaQuery.of(context).size.width * 0.05,
-            right: this.navBarEssentials!.padding?.right ??
-                MediaQuery.of(context).size.width * 0.05,
-            bottom: this.navBarEssentials!.padding?.bottom ??
-                this.navBarEssentials!.navBarHeight! * 0.1),
-        child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                AnimatedContainer(
+    return Container(
+      decoration: BoxDecoration(color: Color(0xfff4faff).withOpacity(0.88)),
+      width: double.infinity,
+      height: this.navBarEssentials!.navBarHeight,
+      padding: EdgeInsets.only(
+          top: this.navBarEssentials!.padding?.top ?? 0.0,
+          left: this.navBarEssentials!.padding?.left ??
+              MediaQuery.of(context).size.width * 0.05,
+          right: this.navBarEssentials!.padding?.right ??
+              MediaQuery.of(context).size.width * 0.05,
+          bottom: this.navBarEssentials!.padding?.bottom ??
+              this.navBarEssentials!.navBarHeight! * 0.1),
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              AnimatedContainer(
+                duration:
+                    this.navBarEssentials!.itemAnimationProperties?.duration ??
+                        Duration(milliseconds: 300),
+                curve: this.navBarEssentials!.itemAnimationProperties?.curve ??
+                    Curves.ease,
+                color: Colors.transparent,
+                width: (this.navBarEssentials!.selectedIndex == 0
+                        ? MediaQuery.of(context).size.width * 0.0
+                        : itemWidth * this.navBarEssentials!.selectedIndex!) +
+                    (itemWidth - 80) / 2,
+                height: 2.0,
+              ),
+              Flexible(
+                child: AnimatedContainer(
                   duration: this
                           .navBarEssentials!
                           .itemAnimationProperties
@@ -115,69 +122,51 @@ class BottomNavStyleWTS1 extends StatelessWidget {
                   curve:
                       this.navBarEssentials!.itemAnimationProperties?.curve ??
                           Curves.ease,
-                  color: Colors.transparent,
-                  width: (this.navBarEssentials!.selectedIndex == 0
-                          ? MediaQuery.of(context).size.width * 0.0
-                          : itemWidth * this.navBarEssentials!.selectedIndex!) +
-                      (itemWidth - 80) / 2,
+                  width: 80,
                   height: 2.0,
-                ),
-                Flexible(
-                  child: AnimatedContainer(
-                    duration: this
-                            .navBarEssentials!
-                            .itemAnimationProperties
-                            ?.duration ??
-                        Duration(milliseconds: 300),
-                    curve:
-                        this.navBarEssentials!.itemAnimationProperties?.curve ??
-                            Curves.ease,
-                    width: 80,
-                    height: 2.0,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: selectedItemActiveColor,
-                      borderRadius: BorderRadius.circular(100.0),
-                    ),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: selectedItemActiveColor,
+                    borderRadius: BorderRadius.circular(100.0),
                   ),
-                )
-              ],
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 5.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: this.navBarEssentials!.items!.map((item) {
-                    int index = this.navBarEssentials!.items!.indexOf(item);
-                    return Flexible(
-                      child: GestureDetector(
-                        onTap: () {
-                          if (this.navBarEssentials!.items![index].onPressed !=
-                              null) {
-                            this.navBarEssentials!.items![index].onPressed!(this
-                                .navBarEssentials!
-                                .selectedScreenBuildContext);
-                          } else {
-                            this.navBarEssentials!.onItemSelected!(index);
-                          }
-                        },
-                        child: Container(
-                          color: Colors.transparent,
-                          child: _buildItem(
-                              item,
-                              this.navBarEssentials!.selectedIndex == index,
-                              this.navBarEssentials!.navBarHeight),
-                        ),
-                      ),
-                    );
-                  }).toList(),
                 ),
+              )
+            ],
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 5.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: this.navBarEssentials!.items!.map((item) {
+                  int index = this.navBarEssentials!.items!.indexOf(item);
+                  return Flexible(
+                    child: GestureDetector(
+                      onTap: () {
+                        if (this.navBarEssentials!.items![index].onPressed !=
+                            null) {
+                          this.navBarEssentials!.items![index].onPressed!(this
+                              .navBarEssentials!
+                              .selectedScreenBuildContext);
+                        } else {
+                          this.navBarEssentials!.onItemSelected!(index);
+                        }
+                      },
+                      child: Container(
+                        color: Colors.transparent,
+                        child: _buildItem(
+                            item,
+                            this.navBarEssentials!.selectedIndex == index,
+                            this.navBarEssentials!.navBarHeight),
+                      ),
+                    ),
+                  );
+                }).toList(),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
